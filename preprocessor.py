3ab7a545a4e473749e837c1b469dfec3952205f2
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-04-23 01:49:07 +0530

    this files performs nlp preprocessing

---
+++ preprocessor.py
@@ -0,0 +1,24 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def extract_keywords(text: str) -> set[str]:
+    doc = nlp(text)
+
+    keywords = set()
+
+    for token in doc:
+        if token.pos_ in ["NOUN", "PROPN", "ADJ"]:
+            keywords.add(token.lemma_.lower())
+
+    return keywords
+
+
+def extract_phrases(text: str, keywords: str) -> list[str]:
+    doc = nlp(text)
+
+    phrases = []
+    phrases = [chunk.text.lower() for chunk in doc.noun_chunks]
+
+    return list(filter(lambda phrase: phrase not in keywords, phrases))
