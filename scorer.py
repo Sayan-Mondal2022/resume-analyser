e4c4ceca4a6f8c3a82266aba166ebcc3dbbe3769
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-04-23 13:56:05 +0530

    added type casting

--- scorer.py
+++ scorer.py
@@ -5,28 +5,29 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class Scorer:
 
-    def __init__(self, phrase_weight=0.7, keyword_weight=0.3):
+    def __init__(self, phrase_weight=0.7, keyword_weight=0.3) -> None:
         self.phrase_weight = phrase_weight
         self.keyword_weight = keyword_weight
 
     def get_score(
         self, resume_embeddings: list[ndarray], jd_embeddings: list[ndarray]
-    ) -> float32:
+    ) -> float:
         score_matrix = cosine_similarity(resume_embeddings, jd_embeddings)
 
         best_matches = np.max(score_matrix, axis=1)
 
-        return best_matches.mean()
+        return float(best_matches.mean())
 
-    def keyword_score(self, resume_keywords: set[str], jd_keywords: set[str]):
+    def keyword_score(self, resume_keywords: set[str], jd_keywords: set[str]) -> float:
         common_skills = resume_keywords & jd_keywords
         score = len(common_skills) / len(jd_keywords)
-        return score
+        return float(score)
 
-    def final_score(self, phrase_score: float32, keyword_score: float32) -> float32:
-        return self.phrase_weight * phrase_score + self.keyword_weight * keyword_score
+    def final_score(self, phrase_score: float32, keyword_score: float32) -> float:
+        score = self.phrase_weight * phrase_score + self.keyword_weight * keyword_score
+        return float(score)
 
-    def interpret(self, score):
+    def interpret(self, score) -> str:
         if score >= 0.8:
             return "Excellent Match"
         elif score >= 0.6:
